33d19f20caf01122a728b6d6e21a4a1528a23a7a
vikashasm <vikashboora10270@gmail.com>
2022-07-15 14:49:29 +0530

    error solved static files

--- weather_app/settings.py
+++ weather_app/settings.py
@@ -72,7 +72,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'weather_app.wsgi.application'
 
-STATICFILES_DIRS = [BASE_DIR / "static",]
+
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 
 
 # Default primary key field type
