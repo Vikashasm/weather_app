bcf5f250f4b7b65007f48c3a2109ac0cf1a32afc
vikashasm <vikashboora10270@gmail.com>
2022-07-16 13:42:56 +0530

    shortout bugsd

--- weather_app/settings.py
+++ weather_app/settings.py
@@ -22,11 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ['SECRET_KEY']
-SECRET_KEY ="django-insecure-z+$2cp_)c3%$c2v(sub(zidgbtec=5yrr^-6eee@2#vzgqe7%f"
+SECRET_KEY = os.environ['SECRET_KEY']
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
